39bcf56b767492439f9f79a5ea6894386afd4632
Juan Carlos Teso <jcarlos.teso@gmail.com>
2023-02-08 15:10:47 +0100

    No way, Jose

--- zicl/src/zicl/exit.clj
+++ zicl/src/zicl/exit.clj
@@ -1,6 +1,7 @@
 (ns zicl.exit)
 
-(defmacro TO [room-key] `{:to ~room-key})
+(defn TO [room-key] {:to room-key})
+(defn SORRY [message] {:sorry message})
 
 (defmacro EXIT [direction-key exit-spec]
   `(fn [room#]
@@ -10,49 +11,49 @@
              (assoc {} ~direction-key ~exit-spec))))
 
 (defmacro NORTH
-  ([room-key] `(NORTH TO ~room-key))
+  ([arg] `(NORTH (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :north (~exit-fn ~room-key))))
 
 (defmacro NE
-  ([room-key] `(NE TO ~room-key))
+  ([arg] `(NE (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :north-east (~exit-fn ~room-key))))
 
 (defmacro EAST
-  ([room-key] `(EAST TO ~room-key))
+  ([arg] `(EAST (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :east (~exit-fn ~room-key))))
 
 (defmacro SE
-  ([room-key] `(SE TO ~room-key))
+  ([arg] `(SE (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :south-east (~exit-fn ~room-key))))
 
 (defmacro SOUTH
-  ([room-key] `(SOUTH TO ~room-key))
+  ([arg] `(SOUTH (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :south (~exit-fn ~room-key))))
 
 (defmacro SW
-  ([room-key] `(SW TO ~room-key))
+  ([arg] `(SW (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :soutw-west (~exit-fn ~room-key))))
 
 (defmacro WEST
-  ([room-key] `(WEST TO ~room-key))
+  ([arg] `(WEST (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :west (~exit-fn ~room-key))))
 
 (defmacro NW
-  ([room-key] `(NW TO ~room-key))
+  ([arg] `(NW (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :north-west (~exit-fn ~room-key))))
 
 (defmacro UP
-  ([room-key] `(UP TO ~room-key))
+  ([arg] `(UP (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :up (~exit-fn ~room-key))))
 
 (defmacro DOWN
-  ([room-key] `(DOWN TO ~room-key))
+  ([arg] `(DOWN (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :down (~exit-fn ~room-key))))
 
 (defmacro IN
-  ([room-key] `(IN TO ~room-key))
+  ([arg] `(IN (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :in (~exit-fn ~room-key))))
 
 (defmacro OUT
-  ([room-key] `(OUT TO ~room-key))
+  ([arg] `(OUT (if (keyword? ~arg) TO SORRY) ~arg))
   ([exit-fn room-key] `(EXIT :out (~exit-fn ~room-key))))
--- zicl/test/test/zicl/exit.clj
+++ zicl/test/test/zicl/exit.clj
@@ -1,16 +1,32 @@
 (ns test.zicl.exit
   (:require [clojure.test :refer [are deftest function? testing]]
             [test.helpers :refer [have-key?]]
-            [zicl.exit :refer [EAST WEST TO]]))
+            [zicl.exit :refer [EAST NORTH SORRY SOUTH TO WEST]]))
+
+(def ^:private nexit-message "There's a reason why you can't go that way.")
 
 (deftest test-exit
   (let [east-uexit (EAST :somewhere)
-        west-uexit (WEST TO :somewhere)]
+        west-uexit (WEST TO :somewhere)
+        north-nexit (NORTH nexit-message)
+        south-nexit (SOUTH SORRY nexit-message)]
     (testing "Exits are prop functions"
       (are [exit] (function? exit)
         east-uexit
-        west-uexit))
+        west-uexit
+        north-nexit
+        south-nexit))
     (testing "Add an :exits key to the room"
       (have-key? :exits
                  (east-uexit {})
-                 (west-uexit {})))))
+                 (west-uexit {})
+                 (north-nexit {})
+                 (south-nexit {})))
+    (testing "Unconditional exits"
+      (are [exit key] (= {:to :somewhere} (get-in exit [:exits key]))
+        (east-uexit {}) :east
+        (west-uexit {}) :west))
+    (testing "Non-exits"
+      (are [exit key] (= {:sorry nexit-message} (get-in exit [:exits key]))
+        (north-nexit {}) :north
+        (south-nexit {}) :south))))
